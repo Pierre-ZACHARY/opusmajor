2ab842187ea0c8b53cff8831eae01326ca0368a7
Pierre ZACHARY <pierre.zachary45@gmail.com>
2026-06-28 22:19:29 +0200

    added a dedicated health check route

--- src/main.go
+++ src/main.go
@@ -9,6 +9,12 @@ import (
 func main() {
 	router := gin.Default()
 
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
 	router.GET("/player-data", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"player": "demo-player",
